b3427148f572e26d1ab9fd896626f532238cb81f
TakkiyaHendry <44971819+TakkiyaHendry@users.noreply.github.com>
2019-01-31 17:25:28 +0700

    Add files via upload

---
+++ config.php
@@ -0,0 +1,24 @@
+<?php
+// configurasi //
+
+
+// user_name boleh kosong //
+$user_name="";
+
+//user_gaid wajib isi
+$user_gaid= "a94869c3-6e7f-42ff-bd52-754959ff0d6d";
+//user_token wajib di isi //
+$user_token="AbwRPtsl6tFebeBl";
+
+//user_id wajib di isi //
+$user_id="Facebook_1131904113657377";
+
+// invite_code wajib di isi //
+$invite_code="FAJV1pJo8m";
+
+// Bearer wajib di isi //
+$Bearer="eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9";
+
+
+
+?>
